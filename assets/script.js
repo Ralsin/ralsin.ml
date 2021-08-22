c407d31d1cd8e5ce93f851f6c5d55a2c203b660f
Ralsi <ralseineon@outlook.com>
2021-08-22 22:24:44 +0300

    webpage progress #1

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+function href(page){
+    switch (page) {
+        case 'test': location.href='https://ralsin.github.io/fnf/note-swapper'; break;
+        case 'github': location.href='https://github.com/Ralsin'; break;
+    }
+}
